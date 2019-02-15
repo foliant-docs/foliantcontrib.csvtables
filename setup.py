28c10c7f0da1268cbb361f541664c2dd1b66a41d
Anton Bukhtiyarov <a.buhtiyarov@restream.rt.ru>
2019-02-15 13:42:25 +0300

    setup.py fixed

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     description=SHORT_DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
-    version='1.0.0',
+    version='1.0.1',
     author='Anton Bukhtiyarov',
     author_email='apkraft@gmail.com',
     url='https://github.com/foliant-docs/foliantcontrib.csvtables',
@@ -24,7 +24,7 @@ setup(
     license='MIT',
     platforms='any',
     install_requires=[
-        'foliant>=1.0.8'
+        'foliant>=1.0.8',
         'foliantcontrib.utils.combined_options>=1.0.0'
     ],
     classifiers=[
